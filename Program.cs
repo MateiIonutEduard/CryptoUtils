0d9221abdfe02ca2d78d40b26ba0840100be61bb
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-05 18:57:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace CryptoUtils
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
