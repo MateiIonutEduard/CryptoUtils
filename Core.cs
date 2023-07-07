03ab210ce7bd63c5cf56a9a1bf143eae67578c26
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-07 11:25:06 +0300

    Add math features

---
+++ Core.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoUtils
+{
+    public class Core
+    {
+        static bool atkin = false;
+        static bool search = false;
+        static bool escape = false;
+
+        static int gcd(int a, int b)
+        {
+            if (a == 1 || b == 1) return 1;
+
+            while (b != 0)
+            {
+                int r = a % b;
+                a = b;
+                b = r;
+            }
+
+            return a;
+        }
+
+        static int phi(int n)
+        {
+            int r = 0;
+
+            for (int c = 1; c < n; c++)
+                if (gcd(c, n) == 1) r++;
+
+            return r;
+        }
+    }
+}
